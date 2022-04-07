3253824502f5f23df199725ae46534e3b69417dd
ahmed aouari <ahmed.aouari@gmail.comm>
2022-04-07 22:40:15 +0000

    activiyte DAO

--- src/main/java/com/hdm/gestionCars/DAO/ActivityDAO.java
+++ src/main/java/com/hdm/gestionCars/DAO/ActivityDAO.java
@@ -18,7 +18,7 @@ public interface ActivityDAO extends JpaRepository<Activity, Long> {
 	@Query(value = "SELECT MAX(a.price), a.entreprise_key, a.activity_id, a.activity_date FROM Activity a LEFT JOIN Entreprise e ON a.entreprise_key = e.entreprise_id WHERE a.entreprise_key= :entrepriseId", nativeQuery = true)
 	List<Object> listOfActivitiesFromTheDBMax(@Param(value = "entrepriseId") Long entrepriseId);
 
-	@Query(value = "SELECT a.entreprise.entrepriseId FROM Activity a WHERE a.price=(SELECT MAX(price) FROM Activity a2 WHERE a.car.carId = ?1)")
+	@Query(value = "SELECT a.entreprise.entrepriseId FROM Activity a WHERE a.price=(SELECT MAX(a2.price) FROM Activity a2 WHERE a2.car.carId = ?1)")
 	Integer findMaxActivityByCar(Integer carId);
 
 }
